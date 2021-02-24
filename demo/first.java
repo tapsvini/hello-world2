19a4dbb6373b5f5f2c22832654f3d5165b6d8e41
tapsvini.shingala <tapsvini.shingala@gmail.com>
2021-02-24 12:50:49 -0500

    Test message

---
+++ demo/first.java
@@ -0,0 +1,20 @@
+package demo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+import demo.DeleteLastNode.Node;
+
+// Java Implementation of the approach 
+
+public class First{ 
+    // Driver Code 
+    public static void main(String[] args) 
+    { 
+    	
+        System.out.println("\nInorder traversal ");//this is a change//
+        
+    } 
+    }
